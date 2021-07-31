8573fa714d7c4243680b08fd230ccc323ce29169
John Bentley <jwbentle@gmail.com>
2021-07-31 13:42:22 -0700

    Temporarily remove navigation and fix images

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
+/**
+ * @file _app.js
+ */
+
+// Import dependencies
+import React, { useEffect } from 'react';
+import App, { Container } from 'next/app';
+import TagManager from "react-gtm-module"
+
+// Import styles
 import '../styles/globals.css';
 
+const tagManagerArgs = {
+  id: "GTM-M9ZGKXX",
+}
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+
   return <Component {...pageProps} />
 }
 
--- pages/index.js
+++ pages/index.js
@@ -32,21 +32,6 @@ export default function Home({ heroContent, leftRightTextContent, footerContent 
           />
           <meta name="theme-color" content="#f9C586" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          {/* Google Tag Manager */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `[
-                <!-- Google Tag Manager -->
-                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-M9ZGKXX');</script>
-                <!-- End Google Tag Manager -->
-              ]`
-            }}
-          />
-          {/* End Google Tag Manager */}
         </Head>
 
         <Navigation />

b59e3b7d570e3a31782a45749dc3b241ae69b5b9
John Bentley <jwbentle@gmail.com>
2022-03-22 16:00:44 -0400

    Add Peaches picture and update preview image

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,32 @@
 // Import dependencies
-import { useEffect } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import TagManager from 'react-gtm-module';
+import { useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import TagManager from "react-gtm-module";
 
 // Import components
-import Hero from '../components/hero';
-import Navigation from '../components/navigation';
-import LeftRightText from '../components/left-right-text';
-import BorderLeft from '../components/border-left';
-import ImageBlock from '../components/image-block';
-import Footer from '../components/footer';
+import Hero from "../components/hero";
+import Navigation from "../components/navigation";
+import LeftRightText from "../components/left-right-text";
+import BorderLeft from "../components/border-left";
+import ImageBlock from "../components/image-block";
+import Footer from "../components/footer";
 import RSVPForm from "../components/rsvp-form";
 
 // Import styles
-import { pageLayoutStyles } from '../utils/constants';
+import { pageLayoutStyles } from "../utils/constants";
 
 export default function Home() {
   useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-M9ZGKXX' });
+    TagManager.initialize({ gtmId: "GTM-M9ZGKXX" });
 
-    let circle = document.getElementById('circle');
-    const onMouseMove = (e) =>{
-      circle.style.left = (e.pageX / 2) + 'px';
-      circle.style.top = (e.pageY / 2) + 'px';
+    let circle = document.getElementById("circle");
+    const onMouseMove = (e) => {
+      circle.style.left = e.pageX / 2 + "px";
+      circle.style.top = e.pageY / 2 + "px";
       console.log("mouse position: " + e.pageX);
-    }
-    document.addEventListener('mousemove', onMouseMove);
+    };
+    document.addEventListener("mousemove", onMouseMove);
   }, []);
 
   return (
@@ -42,15 +42,19 @@ export default function Home() {
             content={`John and Geovana are getting married! Come celebrate with us in Portland on 7-29-2022.`}
           />
           <meta name="theme-color" content="#f9C586" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta property="og:image" content="/images/geo-ring.webp" />
         </Head>
 
         <Navigation />
-        
-        <Hero/>
+
+        <Hero />
 
         <div id={`circle`}>
-          <Image 
+          <Image
             src={`/images/blur-circle--purple.png`}
             layout={`intrinsic`}
             height={550}
@@ -67,18 +71,25 @@ export default function Home() {
         <LeftRightText />
       </div>
 
-      
-
       <div className={`${pageLayoutStyles}`}>
         {/* <RSVPForm /> */}
-        <Footer showSeeYouThere={true}/>
+        <Image
+          src={`/images/peaches.webp`}
+          layout={`intrinsic`}
+          height={667}
+          width={1000}
+          alt={`Peaches standing in grass looking at the camera.`}
+          loading={'lazy'}
+          lazyBoundary={400}
+        />
+        <Footer showSeeYouThere={true} />
       </div>
     </>
-  )
+  );
 }
 
 export async function getStaticProps(context) {
   return {
-    props: {}
-  }
+    props: {},
+  };
 }
